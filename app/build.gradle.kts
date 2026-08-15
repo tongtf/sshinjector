5d35e8b7ab202aa06df92e0cf5de97fc74afe59e
tongtengfei <tongtf@qq.com>
2026-08-16 02:22:18 +0800

    chore: bump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "cn.srv0.sshinjector"
         minSdk = 34
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
