2f0b164cfaa4e8c6f239ce6777f7ffa6e891a062
tongtengfei <tongtf@qq.com>
2026-08-28 17:46:21 +0800

    chore(deps): apply resolutionStrategy to subprojects for full dependency force

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,3 +24,23 @@ buildscript {
         }
     }
 }
+
+subprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("org.bouncycastle:bcprov-jdk18on:1.85.2")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
+            force("org.bouncycastle:bcutil-jdk18on:1.85")
+            force("com.google.protobuf:protobuf-java:4.35.1")
+            force("org.apache.commons:commons-compress:1.27.1")
+            force("org.apache.commons:commons-lang3:3.18.0")
+            force("commons-io:commons-io:2.17.0")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.jdom:jdom2:2.0.6.1")
+            force("org.apache.httpcomponents:httpclient:4.5.14")
+            force("org.apache.httpcomponents:httpcore:4.4.16")
+            force("org.apache.httpcomponents:httpmime:4.5.14")
+            force("com.google.protobuf:protobuf-java-util:4.35.1")
+        }
+    }
+}
